a8d0935e0ffc1d0971259af43cf338231e8a41e2
eklavya sanmotra <eklavyasanmotra@eklavyas-MacBook-Pro.local>
2023-06-17 15:17:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+      {/* Main content goes here */}
+    </main>
+    <footer>
+      {/* Footer content goes here */}
+    </footer>
+    </div>
+  );
+}
+
+export default App;
